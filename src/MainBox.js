ae6a34f0a689e8a7691fe53b22cf51eb4747d88b
Caffretro <caffretro@gmail.com>
2020-08-03 23:24:50 -0500

    added constructor to Mainbox

--- src/MainBox.js
+++ src/MainBox.js
@@ -1,6 +1,17 @@
 import React from "react";
-import {render} from "react-dom";
+import { render } from "react-dom";
 
 class MainBox extends React.Component {
-    　
+    container(props) {
+        super(props);
+        this.state = {
+            category: Array(7).fill(null), // category[0] is default that displays random items
+            displayingCategory: 0,
+        }
+    }
+    categoryBoxHandler(i) {
+        this.setState({
+            displayingCategory: i
+        })
+    }
 }
