66a56b185e4da64ea293c4c4c0987b98171d3c0b
Caffretro <caffretro@gmail.com>
2020-08-03 23:56:01 -0500

    started categorybox

---
+++ src/CategoryBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+
+class CategoryBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // string array，商品类别tag用
+        }
+    }
+    render() {
+        return (
+            <div className={"CategoryBox"}>
+
+            </div>
+        )
+    }
+}
